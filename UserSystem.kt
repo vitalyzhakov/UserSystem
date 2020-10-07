fcedf7c97a493ceff524f1f97db609d862837bee
Zhakov <vitalyzhakov@gmail.com>
2020-10-08 00:05:05 +0500

    move to class

--- UserSystem.kt
+++ UserSystem.kt
@@ -2,32 +2,35 @@ fun main(args: Array<String>) {
     SayHello(args)
 }
 
-fun SayHello(args: Array<String>)
-{
-    val systemName = System.getProperty("os.name")
-    val userName = System.getProperty("user.name")
-    var systemRequired = false
-    var userRequired = false
-    for ( argument in args )
-    {
-        when (argument)
-        {
-            "-u" -> userRequired = true
-            "-s" -> systemRequired = true
-            else -> println("Program has unknown argument " + argument)
-        }
-    }
+class SayHello {
 
-    var result = "Hello!"
-    if ( systemRequired && userRequired)
+    constructor(args: Array<String>)
     {
-        result = "Hello, " + userName + " at " + systemName + " system!"
-    } else if ( userRequired )
-    {
-        result = " Hello, " + userName + ")"
-    } else if ( systemRequired)
-    {
-        result = "Hello at " + systemName + "!"
+        val systemName = System.getProperty("os.name")
+        val userName = System.getProperty("user.name")
+        var systemRequired = false
+        var userRequired = false
+        for ( argument in args )
+        {
+            when (argument)
+            {
+                "-u" -> userRequired = true
+                "-s" -> systemRequired = true
+                else -> println("Program has unknown argument " + argument)
+            }
+        }
+
+        var result = "Hello!"
+        if ( systemRequired && userRequired)
+        {
+            result = "Hello, " + userName + " at " + systemName + " system!"
+        } else if ( userRequired )
+        {
+            result = " Hello, " + userName + ")"
+        } else if ( systemRequired)
+        {
+            result = "Hello at " + systemName + "!"
+        }
+        println(result);
     }
-    println( result )
 }
